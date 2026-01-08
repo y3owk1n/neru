dbaef47c0a01b41859a9d255666c953ed4657951
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-01-09 00:01:41 +0800

    chore: bump flake to 1.14.0 (#297)

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       ];
 
       # Update this to your latest release version
-      latestVersion = "1.13.0";
+      latestVersion = "1.14.0";
 
       # Function to build package with specific version
       makeNeruPackage =
