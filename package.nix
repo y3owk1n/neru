806c770a1177cdd56635ba55d79942cd5628e797
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-02-08 20:01:46 +0800

    chore: bump flake to 1.18.0

--- package.nix
+++ package.nix
@@ -21,13 +21,13 @@ if useZip then
       {
         "aarch64-darwin" = {
           url = "https://github.com/y3owk1n/neru/releases/download/v${version}/neru-darwin-arm64.zip";
-          # run `nix hash convert --hash-algo sha256 (nix-prefetch-url --unpack https://github.com/y3owk1n/neru/releases/download/v1.17.1/neru-darwin-arm64.zip)`
-          sha256 = "sha256-24BbAcjpbQ6BhsV3kqk3mbAeswDZ43wtdAEGUidO9a0=";
+          # run `nix hash convert --hash-algo sha256 (nix-prefetch-url --unpack https://github.com/y3owk1n/neru/releases/download/v1.18.0/neru-darwin-arm64.zip)`
+          sha256 = "sha256-UewO+0bJ4fbjhy4Zlb4x65ALMUuEDj9qKlP+oJGsN0w=";
         };
         "x86_64-darwin" = {
           url = "https://github.com/y3owk1n/neru/releases/download/v${version}/neru-darwin-amd64.zip";
-          # run `nix hash convert --hash-algo sha256 (nix-prefetch-url --unpack https://github.com/y3owk1n/neru/releases/download/v1.17.1/neru-darwin-amd64.zip)`
-          sha256 = "sha256-+g5LcKA3DEsx0gGE57puMauZ6x9x8bmmEEdV3OUZDN4=";
+          # run `nix hash convert --hash-algo sha256 (nix-prefetch-url --unpack https://github.com/y3owk1n/neru/releases/download/v1.18.0/neru-darwin-amd64.zip)`
+          sha256 = "sha256-Ah3+9AyRa45f14grUOjeyxp7yxvQZsem7ibodsQ5ciI=";
         };
       }
       .${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
