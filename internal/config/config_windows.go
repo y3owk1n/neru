07a8e1adf4387b67fe0b5c10280e66b5ab61797c
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-03-12 14:46:17 +0800

    fix: remove explorer.exe and Taskmgr.exe from Windows default excluded apps

--- internal/config/config_windows.go
+++ internal/config/config_windows.go
@@ -3,12 +3,6 @@
 package config
 
 func applyPlatformDefaults(cfg *Config) {
-	// Windows-specific defaults
-	cfg.General.ExcludedApps = append(cfg.General.ExcludedApps,
-		"explorer.exe",
-		"Taskmgr.exe",
-	)
-
 	// UIA control type roles for clickable elements
 	cfg.Hints.ClickableRoles = append(cfg.Hints.ClickableRoles,
 		"Button",
