d38e5c8981e759c86fd91f6f93206ddb4e226167
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-03-19 10:44:58 +0800

    fix(config): remove unnecessary defaults for exclusions (#612)
    * fix(config): remove unnecessary defaults for exclusions
    
    * fix(config): remove the same for linux stub

--- internal/config/config_linux.go
+++ internal/config/config_linux.go
@@ -4,12 +4,6 @@ package config
 
 func applyPlatformDefaults(cfg *Config) {
 	// Linux-specific defaults
-	cfg.General.ExcludedApps = append(cfg.General.ExcludedApps,
-		"org.gnome.Shell",
-		"org.kde.kwin",
-		"org.kde.plasmashell",
-	)
-
 	cfg.Hints.ClickableRoles = append(cfg.Hints.ClickableRoles,
 		"push button",
 		"check box",
