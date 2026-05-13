017acaddc99d5f8adc03b7896874ca6bc3caaf93
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-05-13 16:36:20 +0800

    chore: bump deps to latest

--- nix/package.nix
+++ nix/package.nix
@@ -162,7 +162,7 @@ else
     # `nix-shell -p go --run 'go mod vendor'`
     # `nix hash path vendor`
     # `rm -rf vendor`
-    vendorHash = "sha256-SCZUfw5Fx9k2jLTKmD42cXldJJuwwbz5pgQPVU6niLU=";
+    vendorHash = "sha256-/sAoUAnue3j0c7XSlQLIPte9OV6uVqveWtOF9VA7uyc=";
 
     ldflags = [
       "-s"
