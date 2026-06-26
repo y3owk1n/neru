f51d0f4a2a8503644f295b05e1d86b1ebfb2dd4d
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-06-26 19:16:00 +0800

    fix: register systray quit on windows

--- internal/app/initialization_platform_windows.go
+++ internal/app/initialization_platform_windows.go
@@ -2,10 +2,16 @@
 
 package app
 
-import "go.uber.org/zap"
+import (
+	"go.uber.org/zap"
+
+	infrasystray "github.com/y3owk1n/neru/internal/core/infra/systray"
+)
 
 // initializePlatformLogger is a no-op on Windows.
 func initializePlatformLogger(_ *zap.Logger) {}
 
-// platformQuit is a no-op on Windows.
-func platformQuit() {}
+// platformQuit posts WM_QUIT to unblock the Windows message pump.
+func platformQuit() {
+	infrasystray.Quit()
+}
