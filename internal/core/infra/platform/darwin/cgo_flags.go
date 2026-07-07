0d7efeaca113733ac27a2f025b2347289b7770c1
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-07-07 14:17:47 +0800

    fix(darwin,ci): pin arm64 CI runner & enforfe macos 14 minimum

--- internal/core/infra/platform/darwin/cgo_flags.go
+++ internal/core/infra/platform/darwin/cgo_flags.go
@@ -3,7 +3,7 @@
 package darwin
 
 /*
-#cgo CFLAGS: -x objective-c -fobjc-arc
+#cgo CFLAGS: -x objective-c -fobjc-arc -mmacosx-version-min=14.0
 #cgo LDFLAGS: -framework Foundation -framework AppKit -framework Carbon -framework CoreGraphics -framework ApplicationServices -framework UserNotifications -framework QuartzCore -framework Vision -framework ScreenCaptureKit
 */
 import "C"
