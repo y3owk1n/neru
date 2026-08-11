0f64debd878cce5593fed381151a0de61a0f1ab2
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-08-12 00:57:56 +0800

    chore: fmt

--- internal/cli/docs_other.go
+++ internal/cli/docs_other.go
@@ -5,5 +5,8 @@ package cli
 import "github.com/y3owk1n/neru/internal/derrors"
 
 func openDocsPage(path string) error {
-	return derrors.New(derrors.CodeNotSupported, "open documentation is only implemented for macOS and Linux")
+	return derrors.New(
+		derrors.CodeNotSupported,
+		"open documentation is only implemented for macOS and Linux",
+	)
 }
